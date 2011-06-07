5aa790bd5a4f1ab94a68d499911766ca12026f29
Ari Hershowitz <aih@tabulaw.com>
2011-06-06 23:54:02 -0700

    Added CSS

--- settings.py
+++ settings.py
@@ -67,7 +67,7 @@ MEDIA_ROOT = SETTINGS_ROOT+ "/site_media/"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = "site_media/" 
+MEDIA_URL = "http://localhost:8000/site_media/" 
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
